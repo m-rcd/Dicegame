6dee30a51287a40ee9a5654126960682a5bd4918
Marianne <marianne.rachid@gmail.com>
2018-08-11 15:43:28 +0300

    add same result option

--- dicegame1.rb
+++ dicegame1.rb
@@ -2,7 +2,9 @@ player1 = rand(1..7)
 player2 = rand(1..7)
 
 if player1 > player2
-  puts 'I win'
+  puts 'Player1 wins'
+elsif player1 < player2
+  puts 'Player2 wins'
 else
-  puts 'I lose'
+  puts 'Try again'
 end
