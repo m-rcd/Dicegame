67788216710ead4e3c1e7451cba42693bbf6960a
Marianne <marianne.rachid@gmail.com>
2018-08-11 15:46:26 +0300

    same result option

--- dicegame2.rb
+++ dicegame2.rb
@@ -4,7 +4,9 @@ player2_dice1 = rand(1..7)
 player2_dice2 = rand(1..7)
 
 if player1_dice1 + player1_dice2 > player2_dice1 + player2_dice2
-  puts 'player1 wins'
+  puts 'Player1 wins'
+elsif player1_dice1 + player1_dice2 < player2_dice1 + player2_dice2
+  puts 'Player2 wins'
 else
-  puts 'player2 wins'
+  puts 'Try again'
 end
